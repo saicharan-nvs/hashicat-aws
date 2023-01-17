3c5038f909dffd34e33ddfb52d3302d4b5a08ccf
Sai Charan <kingsthunders@gmail.com>
2023-01-17 18:55:13 +0000

    Added s3 bucket module

--- variables.tf
+++ variables.tf
@@ -9,6 +9,12 @@ variable "prefix" {
   description = "This prefix will be included in the name of most resources."
 }
 
+variable "bucket_prefix" {
+    description =   "saicharan"
+    default =   "null"
+}
+
+
 variable "region" {
   description = "The region where the resources are created."
   default     = "us-east-1"
